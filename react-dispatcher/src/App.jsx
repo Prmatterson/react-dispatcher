ebfc5ff777c4757bc784bf45678f719ab2593527
Prmatterson <prmatterson@protonmail.com>
2023-03-26 10:40:33 -0230

    website loads index.html

--- react-dispatcher/src/App.jsx
+++ react-dispatcher/src/App.jsx
@@ -1,23 +1,11 @@
 import './App.css';
+import GridFramework from '.components/GridFramework'
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>test in App.jsx</p>
+      <GridFramework />
     </div>
   );
 }
-
-export default App;
