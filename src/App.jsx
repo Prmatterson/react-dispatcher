ac4b8b9676f62104a432ea73d0eae3a04e82d7b2
Prmatterson <prmatterson@protonmail.com>
2023-04-02 14:48:14 -0230

    starting GridFramework
    can get basic columns or tiles to load, but defaults vertically (using is-mobile class as per Bulma docs does not seem to fix it)

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      Test App.jsx again
       <GridFramework />
     </div>
   );
--- src/components/GridFramework.jsx
+++ src/components/GridFramework.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 export default function GridFramework() {
-    return (
-        <div>GridFramework Test</div>
-    )
-};
+  return (
+    <div>
+      <div class="columns is-mobile">
+        <p>First Column</p>
+        <div class="column is-mobile">
+          <p>Time</p>
+        <div class="column is-mobile">
+            <p>8.10</p>
+        </div>
+        </div>
+      </div>
+      <div class="columns is-mobile">
+        <p>Second Column</p>
+        <div class="column is-mobile">
+          <p>Tech 1</p>
+        </div>
+      </div>
+    </div>
+  );
+}
