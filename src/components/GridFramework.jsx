bc0ce0bf716cdef9f3e4e4f2c6e37e50aa3f87e6
Prmatterson <106201570+Prmatterson@users.noreply.github.com>
2023-04-05 20:33:29 -0230

    updating style and grid framework

--- src/components/GridFramework.jsx
+++ src/components/GridFramework.jsx
@@ -2,22 +2,8 @@ import React from "react";
 
 export default function GridFramework() {
   return (
-    <div>
-      <div class="columns is-mobile">
-        <p>First Column</p>
-        <div class="column is-mobile">
-          <p>Time</p>
-        <div class="column is-mobile">
-            <p>8.10</p>
-        </div>
-        </div>
-      </div>
-      <div class="columns is-mobile">
-        <p>Second Column</p>
-        <div class="column is-mobile">
-          <p>Tech 1</p>
-        </div>
-      </div>
-    </div>
+    <>
+      
+    </>
   );
 }
--- src/style.js
+++ src/style.js
@@ -1,24 +1,23 @@
-// .RAW_COLOURS {
-//     teals: [
-//         background-teal:"#0C1A1A";
-//         accent-teal: "#6ACFC7";
-//         text-teal: "#E9F2F1";
-//     ],
-//     reds: [
-//         ruby-red: "#9B111E",
-//     ]
-//  }
+    teals = [
+        background-teal:"#0C1A1A";
+        accent-teal: "#6ACFC7";
+        text-teal: "#E9F2F1";
+    ],
+    reds: [
+        ruby-red: "#9B111E",
+    ]
+ }
  
  const theme = {
     background: {
-        primary: RAW_COLOURS.teals[0],
+        primary: teals[0],
     },
     text: {
-        primary: RAW_COLOURS.teals[2],
-        secondary: RAW_COLOURS.teals[1],
+        primary: teals[2],
+        secondary: teals[1],
     },
     buttons: {
-        primary: RAW_COLOURS.teals[1],
-        negative: RAW_COLOURS.reds[0],
+        primary: teals[1],
+        negative: reds[0],
     }
  }
