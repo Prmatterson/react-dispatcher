fb7f53c118169ac461d1899c3bca379a4e078546
Prmatterson <prmatterson@protonmail.com>
2023-04-12 19:47:19 -0230

    more structure on App.jsx; working on for loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import Header from './components/Header'
-import TimeIntervals from './components/TimeIntervals'
+import Header from "./components/Header";
+import Techs from "./components/Techs";
+import TimeIntervals from "./components/TimeIntervals";
 import React from "react";
+import { Tile } from "react-bulma-components";
 
 export default function App() {
   return (
     <div>
       <Header />
-      <TimeIntervals />
+      <Tile kind="ancestor">
+        <Tile kind="parent" size={1}>
+          <Tile kind="child">
+            <TimeIntervals />
+          </Tile>
+        </Tile>
+        <Tile>
+          <Tile kind="parent" size={11}>
+            <Tile kind="child">
+              <Tile>
+                <Techs />
+              </Tile>
+            </Tile>
+          </Tile>
+        </Tile>
+      </Tile>
     </div>
   );
-};
+}
---
+++ src/components/Techs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import 'bulma/css/bulma.min.css';
+
+export default function Header() {
+  return (
+    <>
+        Test
+    </>
+  );
+}
