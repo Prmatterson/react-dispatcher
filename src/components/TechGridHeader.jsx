be7ffa4b2d8dc5418875c321fffa73c2fa21c457
Prmatterson <prmatterson@protonmail.com>
2023-04-18 20:17:13 -0230

    Update TechGridHeader.jsx

--- src/components/TechGridHeader.jsx
+++ src/components/TechGridHeader.jsx
@@ -2,9 +2,21 @@ import React from "react";
 import { Content } from "react-bulma-components";
 
 export default function TechGridHeader() {
+
+const headerArray = ["Time "]
+const techName = []
+const techTotal = 3
+
+for (let i = 0; i < techTotal; i += 1) {
+    techName.push(i + ' ')
+}
+
+headerArray.push(techName)
+
+
   return (
     <Content>
-        Test
+        {headerArray}
     </Content>
   );
 }
