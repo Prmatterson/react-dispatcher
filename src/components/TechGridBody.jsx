bd625e4b4a2d55709aa55e255b44a7182b13b0c7
Prmatterson <prmatterson@protonmail.com>
2023-04-18 20:42:04 -0230

    Update TechGridBody.jsx

--- src/components/TechGridBody.jsx
+++ src/components/TechGridBody.jsx
@@ -1,12 +1,30 @@
 import React from "react";
-import 'bulma/css/bulma.min.css';
+import { Content } from "react-bulma-components";
 
 export default function TechGridBody() {
+  const techGrid = []
 
+  const techNames = ["Justin", "Nick", "Blake"];
+  
+  const numberOfTechs = techNames.length;
+//  In future, may have an option in a settings menu to change maxNumberOfRows, likely would be an exported function which would be imported here
+  const maxNumberOfRows = 85;
+  const maxNumberOfColumns = numberOfTechs;
+
+  const techGridFunction = () => {
+    for (let i = 0; i < maxNumberOfRows; i += 1) {
+      for (let j = 0; j < maxNumberOfColumns; j += 1) {
+        techGrid.push(j)
+      }
+      techGrid.push(i)
+    }
+  };
 
   return (
-    <>
-          Insert nested for loops here to make grid
-    </>
+    <Content>
+      {techGridFunction}
+      {console.log(maxNumberOfRows)}
+      {console.log(maxNumberOfColumns)}
+    </Content>
   );
 }
