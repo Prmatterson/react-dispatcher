4677598e5f8b5a92f53abd5db75cd444a8a5b2ac
Prmatterson <prmatterson@protonmail.com>
2023-04-18 20:46:27 -0230

    Update TimeIntervals.jsx

--- src/components/TimeIntervals.jsx
+++ src/components/TimeIntervals.jsx
@@ -6,10 +6,12 @@ export default function TimeIntervals() {
   const cells = ["Time"];
   const cellsLimit = 16.4;
 
+  // Generates array of times from start to finish = "i" defines the start, cellsLimit the end
   for (let i = 8.0; i < cellsLimit; i += 0.1) {
     cells.push(i.toFixed(1));
   }
 
+  // Maps function so it shows up vertically with each element on its own line
   const cellsMap = cells.map((time) => <div>{time}</div>);
 
   return (
