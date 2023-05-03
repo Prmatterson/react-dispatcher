b970596a847d23f24c4029a953e5bcfee4896ffe
dylanbrine <dylan.brine@milkmoovement.com>
2023-05-03 09:34:49 -0300

    got draggable library working

--- src/components/TechGridBody.jsx
+++ src/components/TechGridBody.jsx
@@ -1,38 +1,41 @@
-import { React, useCallback } from "react";
-import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
+import { React, useCallback, useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
 
 export default function TechGridBody() {
-  // using useCallback is optional
-  const onBeforeCapture = useCallback(() => {
-    /*...*/
-  }, []);
-  const onBeforeDragStart = useCallback(() => {
-    /*...*/
-  }, []);
-  const onDragStart = useCallback(() => {
-    /*...*/
-  }, []);
-  const onDragUpdate = useCallback(() => {
-    /*...*/
-  }, []);
-  const onDragEnd = useCallback(() => {
-    // the only one that is required
-  }, []);
+  const [items, setItems] = useState([1, 2, 3]);
+  const onDragEnd = (result) => {
+    console.log('result', result);
+    if (!result.destination) return;
+
+    const oldIndex = result.source.index;
+    const newIndex = result.destination.index;
+
+    const newItems = [...items];
+    const [removed] = newItems.splice(oldIndex, 1);
+    newItems.splice(newIndex, 0, removed);
+    setItems(newItems);
+  };
 
   return (
-    <DragDropContext
-      onBeforeCapture={onBeforeCapture}
-      onBeforeDragStart={onBeforeDragStart}
-      onDragStart={onDragStart}
-      onDragUpdate={onDragUpdate}
-      onDragEnd={onDragEnd}
-    >
-      <Droppable>
-        <Draggable>
-          <div>Hello World</div>
-        </Draggable>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId='droppable'>
+        {(provided, snapshot) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {items.map((item, index) => (
+              <Draggable key={item} draggableId={item.toString()} index={index}>
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}>
+                    {item}
+                  </div>
+                )}
+              </Draggable>
+            ))}
+          </div>
+        )}
       </Droppable>
-      <div>Test Hello world</div>
     </DragDropContext>
   );
-  }
+}
