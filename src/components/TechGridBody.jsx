04a4cadb3a4dbe5ce017502fa3dcf31342fb6776
Prmatterson <prmatterson@protonmail.com>
2023-05-09 18:21:54 -0230

    cleaned up top end

--- src/components/TechGridBody.jsx
+++
@@ -1,6 +0,0 @@
-import { React } from 'react';
-import { VerticalList } from './drag-and-drop/VerticalList';
-
-export default function TechGridBody() {
-  return <VerticalList />;
-}
--- src/components/drag-and-drop/VerticalList.jsx
+++ src/components/drag-and-drop/VerticalList.jsx
@@ -3,10 +3,12 @@ import { TechGrid } from "./TechGrid";
 import { CustomDragLayer } from "./CustomDragLayer";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import TimeIntervals from "./TimeIntervals"
 
 export const VerticalList = () => {
   return (
     <DndProvider backend={HTML5Backend}>
+      <TimeIntervals />
       <TechGrid snapToGrid={true} />
       <CustomDragLayer snapToGrid={true} />
     </DndProvider>
