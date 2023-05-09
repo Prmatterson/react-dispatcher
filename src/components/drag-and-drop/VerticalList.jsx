8279e793468ae1326c99c4ae5ccf34c37a160b93
Prmatterson <prmatterson@protonmail.com>
2023-05-09 18:33:18 -0230

    Update VerticalList.jsx

--- src/components/drag-and-drop/VerticalList.jsx
+++ src/components/drag-and-drop/VerticalList.jsx
@@ -3,13 +3,31 @@ import { TechGrid } from "./TechGrid";
 import { CustomDragLayer } from "./CustomDragLayer";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import TimeIntervals from "./TimeIntervals"
+import TimeIntervals from "./TimeIntervals";
+import { Columns } from "react-bulma-components";
+
 
 export const VerticalList = () => {
+
+  const styles = {
+    width: "100%",
+    height: 2100,
+    border: "1px solid black",
+    position: "relative",
+    marginTop: "30px",
+    marginBottom: "30px",
+  };
+
   return (
     <DndProvider backend={HTML5Backend}>
-      <TimeIntervals />
-      <TechGrid snapToGrid={true} />
+      <Columns style={styles}>
+        <Columns.Column size={1}>
+          <TimeIntervals />
+        </Columns.Column>
+        <Columns.Column size={11}>
+          <TechGrid snapToGrid={true} />
+        </Columns.Column>
+      </Columns>
       <CustomDragLayer snapToGrid={true} />
     </DndProvider>
   );
