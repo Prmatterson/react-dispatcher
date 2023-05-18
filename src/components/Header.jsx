98dd1689b1167aadbdcc3d5a0c255ee35d2bcd9a
Prmatterson <prmatterson@protonmail.com>
2023-05-18 19:40:58 -0230

    starting navbuttons and modals

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import 'bulma/css/bulma.min.css';
-import { Heading } from 'react-bulma-components';
+import "bulma/css/bulma.min.css";
+import { Heading } from "react-bulma-components";
+import NavButtons from "./NavButtons";
 
 export default function Header() {
   return (
     <>
-      <Heading textAlign='center'>
-        Dispatcher
-      </Heading>
-      <Heading subtitle textAlign='center'>
+      <Heading textAlign="center">Dispatcher</Heading>
+      <Heading subtitle textAlign="center">
         The best dispatching program you've ever used
       </Heading>
+      <div>
+        <NavButtons />
+      </div>
     </>
   );
 }
---
+++ src/components/NavButtons.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box, Button } from "react-bulma-components";
+import { AddJobModal } from "./AddJobModal"
+
+export default function NavButtons() {
+  return (
+    <>
+    <Box>
+      <Button.Group>
+        <Button onClick={AddJobModal()}>Add Job</Button>
+      </Button.Group>
+    </Box>
+    <br></br>
+    </>
+  );
+}
