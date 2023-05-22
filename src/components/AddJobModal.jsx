15be898cf2858881645c4dacc565b134327a3287
Prmatterson <prmatterson@protonmail.com>
2023-05-22 09:13:20 -0230

    modal created

--- src/components/AddJobModal.jsx
+++ src/components/AddJobModal.jsx
@@ -1,25 +1,29 @@
-import { Form } from "react-bulma-components";
-import { techs, timeCell } from "./drag-and-drop/TechGrid";
+// import { Form, Modal } from "react-bulma-components";
+// import { techs, timeCell } from "./drag-and-drop/TechGrid";
 
-export default function AddJobModal() {
-  return (
-    <Form.Field>
-      <Form.Label>Tech Name</Form.Label>
-      <Form.Control size={2}>
-        <Form.Select>
-          {techs.map((techDetails) => {
-            return <option>{techDetails.name}</option>;
-          })}
-        </Form.Select>
-        <Form.Label>Work Order Number</Form.Label>
-        <Form.Input></Form.Input>
-        <Form.Label>Length of Job</Form.Label>
-        <Form.Select>
-          {timeCell.map((timeDetails) => {
-            return <option>{timeDetails}</option>;
-          })}
-        </Form.Select>
-      </Form.Control>
-    </Form.Field>
-  );
-}
+// export default function AddJobModal() {
+//   return (
+//     <Modal>
+//       <Modal.Content>
+//         <Form.Field>
+//       <Form.Label>Tech Name</Form.Label>
+//       <Form.Control size={2}>
+//         <Form.Select>
+//           {techs.map((techDetails) => {
+//             return <option>{techDetails.name}</option>;
+//           })}
+//         </Form.Select>
+//         <Form.Label>Work Order Number</Form.Label>
+//         <Form.Input></Form.Input>
+//         <Form.Label>Length of Job</Form.Label>
+//         <Form.Select>
+//           {timeCell.map((timeDetails) => {
+//             return <option>{timeDetails}</option>;
+//           })}
+//         </Form.Select>
+//       </Form.Control>
+//     </Form.Field>
+//     </Modal.Content>
+//     </Modal>
+//   );
+// }
--- src/components/NavButtons.jsx
+++ src/components/NavButtons.jsx
@@ -1,17 +1,42 @@
-import { React } from "react";
-import { Box, Button } from "react-bulma-components";
-import AddJobModal from "./AddJobModal";
+import { React, useState } from "react";
+import { Box, Button, Modal, Form } from "react-bulma-components";
+import { techs, timeCell } from "./drag-and-drop/TechGrid";
 
 export default function NavButtons() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <>
       <Box>
         <Button.Group>
-          <Button onClick={AddJobModal()}>Add Job</Button>
+          <Button onClick={handleShow}>Add Job</Button>
         </Button.Group>
       </Box>
-      <AddJobModal />
+
+      <Modal show={show} onClose={handleClose}>
+        <Modal.Content backgroundColor="white" padding="100px" showClose={true}>
+          <Form.Field>
+            <Form.Label>Tech Name</Form.Label>
+            <Form.Control size={2}>
+              <Form.Select>
+                {techs.map((techDetails) => {
+                  return <option>{techDetails.name}</option>;
+                })}
+              </Form.Select>
+              <Form.Label>Work Order Number</Form.Label>
+              <Form.Input></Form.Input>
+              <Form.Label>Length of Job</Form.Label>
+              <Form.Select>
+                {timeCell.map((timeDetails) => {
+                  return <option>{timeDetails}</option>;
+                })}
+              </Form.Select>
+            </Form.Control>
+          </Form.Field>
+        </Modal.Content>
+      </Modal>
       <br></br>
     </>
   );
