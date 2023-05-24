676055b30a9d69249a6b4952de3878f3caf65874
dylanbrine <dylan.brine@milkmoovement.com>
2023-05-24 16:04:37 -0300

    example of how x snapping could work

--- src/components/drag-and-drop/snapToGrid.js
+++ src/components/drag-and-drop/snapToGrid.js
@@ -1,6 +1,10 @@
 // Changing the fractions changes how much the box moves relative to pointer movement
 export const snapToGrid = (x, y) => {
-  const snappedX = Math.round(x / 1);
+  console.log(`x ${x} y ${y}`);
+
+  const columnWidth = 350;
+  const columnNum = Math.floor(x / columnWidth);
+  const snappedX = columnNum * columnWidth;
   const snappedY = Math.round(y / 24) * 24;
   return [snappedX, snappedY];
 };
