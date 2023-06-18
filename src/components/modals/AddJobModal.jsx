8e6388ea381b82e0673553f2feb1a6a64664a86d
Prmatterson <prmatterson@protonmail.com>
2023-06-18 13:09:06 -0230

    Update AddJobModal.jsx
    not sure what I did, but some data is passing

--- src/components/modals/AddJobModal.jsx
+++ src/components/modals/AddJobModal.jsx
@@ -5,7 +5,7 @@ import { useForm, Controller, FormProvider } from "react-hook-form";
 export function AddJobModal({ show, setShow }) {
   // Setting up state/react-hook-form on form data
   const methods = useForm();
-  const { control, handleSubmit } = methods
+  const { control, handleSubmit } = methods;
 
   const handleClose = () => setShow(false);
 
@@ -13,6 +13,7 @@ export function AddJobModal({ show, setShow }) {
     console.log(data);
     // setAddJobData((prev) => [...prev, data]);
   };
+  const onError = (errors) => console.log(errors);
   // console.log(watch("workOrderNumber")); // watch input value by passing the name of it
 
   // array of times to populate job length form options
@@ -23,103 +24,103 @@ export function AddJobModal({ show, setShow }) {
 
   return (
     <FormProvider {...methods}>
-    <Modal show={show} setShow={setShow} onClose={handleClose}>
-      <Modal.Content backgroundColor="white" showClose={true}>
-        <form onSubmit={handleSubmit(onSubmit)} size={2}>
-          {/* <Form.Label>Tech Name</Form.Label> */}
-          {/* <Form.Select> 
+      <Modal show={show} setShow={setShow} onClose={handleClose}>
+        <Modal.Content backgroundColor="white" showClose={true}>
+          <form onSubmit={handleSubmit(onSubmit, onError)} size={2}>
+            {/* <Form.Label>Tech Name</Form.Label> */}
+            {/* <Form.Select> 
               ---- Add this back in once I figure out how to have it be created within a specific
               column at a specific time (won't be able to overlap other existing jobs)----
                 {techs.map((techDetails) => {
                   return <option>{techDetails.name}</option>;
                 })}
               </Form.Select> */}
-          <Form.Label>Work Order Number</Form.Label>
-          <Controller
-            control={control}
-            name="workOrderNumber"
-            defaultValue=""
-            rules={{ required: true }}
-            render={(field) => (
-              <Form.Input
-                {...field}
-                type="text"
-                name={field.name}
-                onChange={field.onChange}
-                value={field.value}
-              ></Form.Input>
-            )}
-          ></Controller>
-          <Form.Label>Customer Name</Form.Label>
-          <Controller
-            control={control}
-            name="customerName"
-            defaultValue=""
-            rules={{ required: true }}
-            render={(field) => (
-              <Form.Input
-                {...field}
-                type="text"
-                name={field.name}
-                onChange={field.onChange}
-                value={field.value}
-              ></Form.Input>
-            )}
-          ></Controller>
-          <Form.Label>Job Description</Form.Label>
-          <Controller
-            control={control}
-            name="jobDescription"
-            defaultValue=""
-            rules={{ required: true }}
-            render={(field) => (
-              <Form.Input
-                {...field}
-                type="text"
-                name={field.name}
-                onChange={field.onChange}
-                value={field.value}
-              ></Form.Input>
-            )}
-          ></Controller>
-          <Form.Label>Job Length</Form.Label>
-          <Controller
-            control={control}
-            name="jobLength"
-            defaultValue=""
-            rules={{ required: true }}
-            render={(field) => (
-              <Form.Select
-                {...field}
-                name={field.name}
-                onChange={field.onChange}
-                value={field.value}
-              >
-                {jobTime.map((timeDetails) => {
-                  return <option>{timeDetails}</option>;
-                })}
-              </Form.Select>
-            )}
-          ></Controller>
+            <Form.Label>Work Order Number</Form.Label>
+            <Controller
+              control={control}
+              name="workOrderNumber"
+              defaultValue=""
+              rules={{ required: true }}
+              render={(field) => (
+                <Form.Input
+                  {...field}
+                  type="text"
+                  name={field.name}
+                  onChange={field.onChange}
+                  value={field.value}
+                ></Form.Input>
+              )}
+            ></Controller>
+            <Form.Label>Customer Name</Form.Label>
+            <Controller
+              control={control}
+              name="customerName"
+              defaultValue=""
+              rules={{ required: true }}
+              render={(field) => (
+                <Form.Input
+                  {...field}
+                  type="text"
+                  name={field.name}
+                  onChange={field.onChange}
+                  value={field.value}
+                ></Form.Input>
+              )}
+            ></Controller>
+            <Form.Label>Job Description</Form.Label>
+            <Controller
+              control={control}
+              name="jobDescription"
+              defaultValue=""
+              rules={{ required: true }}
+              render={(field) => (
+                <Form.Input
+                  {...field}
+                  type="text"
+                  name={field.name}
+                  onChange={field.onChange}
+                  value={field.value}
+                ></Form.Input>
+              )}
+            ></Controller>
+            <Form.Label>Job Length</Form.Label>
+            <Controller
+              control={control}
+              name="jobLength"
+              defaultValue=""
+              rules={{ required: true }}
+              render={(field) => (
+                <Form.Select
+                  {...field}
+                  name={field.name}
+                  onChange={field.onChange}
+                  value={field.value}
+                >
+                  {jobTime.map((timeDetails) => {
+                    return <option>{timeDetails}</option>;
+                  })}
+                </Form.Select>
+              )}
+            ></Controller>
 
-          <br></br>
-          <br></br>
-          <input
-            className="button"
-            type="submit"
-            renderAs="span"
-            value="Create Job"
-          />
-          <input
-            className="button"
-            type="cancel"
-            renderAs="span"
-            onClick={handleClose}
-            value="Cancel"
-          />
-        </form>
-      </Modal.Content>
-    </Modal>
+            <br></br>
+            <br></br>
+            <input
+              className="button"
+              type="submit"
+              renderAs="span"
+              value="Create Job"
+            />
+            <input
+              className="button"
+              type="cancel"
+              renderAs="span"
+              onClick={handleClose}
+              value="Cancel"
+            />
+          </form>
+        </Modal.Content>
+      </Modal>
     </FormProvider>
   );
 }
