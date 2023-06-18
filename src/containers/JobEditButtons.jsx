ee3ea39be136a6e211d15d91569100969925becc
Dylan B <dylanbrine@gmail.com>
2023-06-18 19:55:33 -0300

    getting the linter to pass

--- src/containers/JobEditButtons.jsx
+++ src/containers/JobEditButtons.jsx
@@ -1,21 +1,17 @@
-import { React, useState } from "react";
-import { Box, Button } from "react-bulma-components";
-import { AddJobModal } from "../components/modals/AddJobModal";
+import { React, useState } from 'react';
+import { Box, Button } from 'react-bulma-components';
+import { AddJobModal } from '../components/modals/AddJobModal';
 // import { techs } from "./drag-and-drop/TechGrid";
 
 export default function JobEditButtons(setAddJobData) {
-
-
   // using state to open/close Modal
-  const [isOpen, setIsOpen] = useState(false);
-  const handleIsOpen = () => setIsOpen(true);
 
   return (
     <>
       {/* Button in toolbar that opens modal */}
       <Box>
         <Button.Group>
-          <Button onClick={handleIsOpen}>Add Job</Button>
+          <Button>Add Job</Button>
         </Button.Group>
       </Box>
       <AddJobModal />
