0f622d2559b4be57b9080ea963ca5e065828caf6
Prmatterson <prmatterson@protonmail.com>
2023-06-20 19:30:33 -0230

    Update AddJobModal.jsx
    when form submitted, object created properly in formData array

--- src/components/modals/AddJobModal.jsx
+++ src/components/modals/AddJobModal.jsx
@@ -1,5 +1,5 @@
 import { React, useState } from "react";
-import { Modal, Form } from "react-bulma-components";
+import { Form } from "react-bulma-components";
 import { useForm, Controller, FormProvider } from "react-hook-form";
 
 export function AddJobModal({ show, setShow }) {
@@ -10,19 +10,45 @@ export function AddJobModal({ show, setShow }) {
     setFormData([...formData, newData]);
   };
 
-  const [inputValue, setInputValue] = useState("");
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  }
+  const [workOrderNumber, setWorkOrderNumber] = useState("");
+  const handleWorkOrderNumberChange = (event) => {
+    setWorkOrderNumber(event.target.value);
+  };
+
+  const [customerName, setCustomerName] = useState("");
+  const handleCustomerNameChange = (event) => {
+    setCustomerName(event.target.value);
+  };
+
+  const [jobDescription, setJobDescription] = useState("");
+  const handleJobDescriptionChange = (event) => {
+    setJobDescription(event.target.value);
+  };
+
+  const [jobLength, setJobLength] = useState("");
+  const handleJobLengthChange = (event) => {
+    setJobLength(event.target.value);
+  };
 
   const handleSubmit = (event) => {
-    addFormData(inputValue);
-  }
+    event.preventDefault();
+    addFormData({
+      workOrderNumber,
+      customerName,
+      jobDescription,
+      jobLength
+    });
+    setWorkOrderNumber("");
+    setCustomerName("");
+    setJobDescription("");
+    setJobLength("");
+  };
 
-  // const onSubmit = (formData) => {
-  //   addFormData(formData);
-  //   console.log(formData);
-  // };
+  // If the below is activated and onSubmit is placed within handleSubmit, a re-render loop is created
+    // const onSubmit = (formData) => {
+    //   addFormData(formData);
+    //   console.log(formData);
+    // };
 
   const handleClose = () => setShow(false);
   const methods = useForm();
@@ -36,107 +62,98 @@ export function AddJobModal({ show, setShow }) {
 
   return (
     <>
-    <FormProvider {...methods}>
-      {/* <Modal show={show} setShow={setShow} onClose={handleClose}> */}
+      <FormProvider {...methods}>
+        {/* <Modal show={show} setShow={setShow} onClose={handleClose}> */}
         {/* <Modal.Content backgroundColor="white" showClose={true}> */}
-          <form onSubmit={handleSubmit} size={2}>
-            {/* <Form.Label>Tech Name</Form.Label> */}
-            {/* <Form.Select> 
-              ---- Add this back in once I figure out how to have it be created within a specific
-              column at a specific time (won't be able to overlap other existing jobs)----
-                {techs.map((techDetails) => {
-                  return <option>{techDetails.name}</option>;
+        <form onSubmit={handleSubmit} size={2}>
+          <Form.Label>Work Order Number</Form.Label>
+          <Controller
+            control={control}
+            name="workOrderNumber"
+            rules={{ required: true }}
+            render={(field) => (
+              <Form.Input
+                {...field}
+                type="text"
+                name={field.name}
+                onChange={handleWorkOrderNumberChange}
+                value={workOrderNumber}
+              ></Form.Input>
+            )}
+          ></Controller>
+          <Form.Label>Customer Name</Form.Label>
+          <Controller
+            control={control}
+            name="customerName"
+            rules={{ required: true }}
+            render={(field) => (
+              <Form.Input
+                {...field}
+                type="text"
+                name={field.name}
+                onChange={handleCustomerNameChange}
+                value={customerName}
+              ></Form.Input>
+            )}
+          ></Controller>
+          <Form.Label>Job Description</Form.Label>
+          <Controller
+            control={control}
+            name="jobDescription"
+            rules={{ required: true }}
+            render={(field) => (
+              <Form.Input
+                {...field}
+                type="text"
+                name={field.name}
+                onChange={handleJobDescriptionChange}
+                value={jobDescription}
+              ></Form.Input>
+            )}
+          ></Controller>
+          <Form.Label>Job Length</Form.Label>
+          <Controller
+            control={control}
+            name="jobLength"
+            rules={{ required: true }}
+            render={(field) => (
+              <Form.Select
+                {...field}
+                name={field.name}
+                onChange={handleJobLengthChange}
+                value={jobLength}
+              >
+                {jobTime.map((timeDetails) => {
+                  return <option>{timeDetails}</option>;
                 })}
-              </Form.Select> */}
-            <Form.Label>Work Order Number</Form.Label>
-            <Controller
-              control={control}
-              name="workOrderNumber"
-              rules={{ required: true }}
-              render={(field) => (
-                <Form.Input
-                  {...field}
-                  type="text"
-                  name={field.name}
-                  onChange={handleInputChange}
-                  value={inputValue}
-                ></Form.Input>
-              )}
-            ></Controller>
-            <Form.Label>Customer Name</Form.Label>
-            <Controller
-              control={control}
-              name="customerName"
-              rules={{ required: true }}
-              render={(field) => (
-                <Form.Input
-                  {...field}
-                  type="text"
-                  name={field.name}
-                  onChange={handleInputChange}
-                  value={inputValue}
-                ></Form.Input>
-              )}
-            ></Controller>
-            <Form.Label>Job Description</Form.Label>
-            <Controller
-              control={control}
-              name="jobDescription"
-              rules={{ required: true }}
-              render={(field) => (
-                <Form.Input
-                  {...field}
-                  type="text"
-                  name={field.name}
-                  onChange={handleInputChange}
-                  value={inputValue}
-                ></Form.Input>
-              )}
-            ></Controller>
-            <Form.Label>Job Length</Form.Label>
-            <Controller
-              control={control}
-              name="jobLength"
-              rules={{ required: true }}
-              render={(field) => (
-                <Form.Select
-                  {...field}
-                  name={field.name}
-                  onChange={handleInputChange}
-                  value={inputValue}
-                >
-                  {jobTime.map((timeDetails) => {
-                    return <option>{timeDetails}</option>;
-                  })}
-                </Form.Select>
-              )}
-            ></Controller>
+              </Form.Select>
+            )}
+          ></Controller>
 
-            <br></br>
-            <br></br>
-            <input
-              className="button"
-              type="submit"
-              renderAs="span"
-              value="Create Job"
-            />
-            <input
-              className="button"
-              type="cancel"
-              renderAs="span"
-              onClick={handleClose}
-              value="Cancel"
-            />
-          </form>
+          <br></br>
+          <br></br>
+          <input
+            className="button"
+            type="submit"
+            renderAs="span"
+            value="Create Job"
+          />
+          <input
+            className="button"
+            type="cancel"
+            renderAs="span"
+            onClick={handleClose}
+            value="Cancel"
+          />
+        </form>
         {/* </Modal.Content> */}
-      {/* </Modal> */}
-    </FormProvider>
-    <div>
-      {formData.map((formDatum) => {
-        return <div>{formData.push(formDatum)}</div> // This renders, but immediately disappears
-      })}
-    </div>
+        {/* </Modal> */}
+      </FormProvider>
+      <div>
+      {formData.map((formDatum, index) => (
+          <div key={index}>{JSON.stringify(formDatum)}</div>
+        ))}
+      </div>
     </>
   );
-
 }
